d469232e81f32e2d18796a453b20069577c2154b
AndresFelipeO <ocampoandres398@gmail.com>
2023-10-17 23:17:49 -0500

    initial: Iniciando el proyecto Android Studio

---
+++ app/src/main/java/com/unicauca/edu/TaskTrakr/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.unicauca.edu.TaskTrakr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.AppTheme
+import com.unicauca.edu.TaskTrakr.view.MyAppNavigationActions
+import com.unicauca.edu.TaskTrakr.view.MyAppRoute
+import com.unicauca.edu.TaskTrakr.view.MyAppTopLevelDestination
+import com.unicauca.edu.TaskTrakr.view.TOP_LEVEL_DESTINATION
+import com.unicauca.edu.TaskTrakr.view.screens.Category
+import com.unicauca.edu.TaskTrakr.view.screens.Settings
+import com.unicauca.edu.TaskTrakr.view.screens.Task
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController= rememberNavController()
+                    val navigateAction= remember(navController){
+                        MyAppNavigationActions(navController)
+                    }
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val selectDestination=navBackStackEntry?.destination?.route?:MyAppRoute.TASK
+                    MyAppContent(
+                        navController = navController,
+                        selectDestination = selectDestination,
+                        navigateTopLevelDestination = navigateAction::navigateTo
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(
+    selectDestination:String,
+    navigateTopLevelDestination:(MyAppTopLevelDestination)->Unit
+) {
+   NavigationBar(
+       modifier = Modifier.fillMaxWidth()
+   ) {
+    TOP_LEVEL_DESTINATION.forEach { destinations ->
+        NavigationBarItem(selected =selectDestination==destinations.route,
+            onClick = { navigateTopLevelDestination(destinations) },
+            icon = {
+                Icon(imageVector = destinations.selectedIcon, 
+                    contentDescription = stringResource(id = destinations.iconTextId))
+            }
+        )
+
+    }
+   }
+}
+
+@Composable
+fun MyAppContent(
+    modifier: Modifier=Modifier,
+    navController: NavHostController,
+    selectDestination: String,
+    navigateTopLevelDestination: (MyAppTopLevelDestination) -> Unit
+){
+    Row(modifier = modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            NavHost(
+                modifier = Modifier.weight(1.0F),
+                navController = navController,
+                startDestination = MyAppRoute.TASK,
+            ){
+                composable(MyAppRoute.TASK){
+                    Task()
+                }
+                composable(MyAppRoute.CATEGORY){
+                    Category()
+                }
+                composable(MyAppRoute.SETTINGS){
+                    Settings()
+                }
+            }
+            MainScreen(selectDestination=selectDestination,
+                navigateTopLevelDestination=navigateTopLevelDestination)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/unicauca/edu/TaskTrakr/view/MyAppTopLevelDestination.kt
@@ -0,0 +1,46 @@
+package com.unicauca.edu.TaskTrakr.view
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import com.unicauca.edu.TaskTrakr.R
+
+
+class MyAppNavigationActions(private val navController: NavHostController) {
+    fun navigateTo(destination: MyAppTopLevelDestination) {
+        navController.navigate(destination.route) {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            launchSingleTop = true
+        }
+    }
+}
+data class MyAppTopLevelDestination(
+    val route:String,
+    val selectedIcon:ImageVector,
+    val iconTextId:Int
+)
+
+val TOP_LEVEL_DESTINATION= listOf(
+   MyAppTopLevelDestination( route=MyAppRoute.TASK,
+       selectedIcon= Icons.Default.DateRange,
+       iconTextId= R.string.tasks),
+    MyAppTopLevelDestination( route=MyAppRoute.CATEGORY,
+        selectedIcon= Icons.Default.Edit,
+        iconTextId= R.string.Categories),
+    MyAppTopLevelDestination( route=MyAppRoute.SETTINGS,
+        selectedIcon= Icons.Default.Settings,
+        iconTextId= R.string.settings)
+)
+
+object MyAppRoute{
+    const val TASK="Task"
+    const val CATEGORY="Category"
+    const val SETTINGS="Settings"
+}
